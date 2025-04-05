427182b457cb5c10c01eedb36e9cbdb2866c4d35
Meraj Siddique <133602950+merajsiddique07@users.noreply.github.com>
2025-04-05 14:01:09 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzRhu1Toml43S5kYzu3z0O68Usv3CZRAI",
+  authDomain: "bunkmodedesigns.firebaseapp.com",
+  projectId: "bunkmodedesigns",
+  storageBucket: "bunkmodedesigns.appspot.com",
+  messagingSenderId: "1051115428515",
+  appId: "1:1051115428515:web:5e0f4e6b46fef6a0dbfd16"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
